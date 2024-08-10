467745728af88495844d5a40d43d6e71b281aab6
rosy <lc_rosy@qq.com>
2024-08-10 09:48:13 +0800

    添加中断功能

--- apps/scheduler_test/src/main.rs
+++ apps/scheduler_test/src/main.rs
@@ -1,28 +1,53 @@
 #![no_std]
 #![no_main]
 
+use core::arch::asm;
+
 use axlog::warn;
 use lazy_init::LazyInit;
+use riscv::register::{sie, sstatus};
 use spinlock::SpinNoIrq;
 use task_management::*;
 use axstd::println;
 
 extern crate alloc;
 use alloc::sync::Arc;
+use trap_handler::CurrentTimebaseFrequency;
+
+struct CurrentTimebaseFrequencyImpl;
+
+#[crate_interface::impl_interface]
+impl CurrentTimebaseFrequency for CurrentTimebaseFrequencyImpl {
+    // 获取dtb上的时基频率比较困难，因此乱填的
+    fn current_timebase_frequency() -> usize {
+        1000_000
+    }
+}
 
 #[no_mangle]
 fn main(cpu_id: usize, cpu_num: usize) {
-    init_main_processor(test_block_wake_yield, cpu_id, cpu_num);
+    trap_handler::init_main_processor();
+    task_management::init_main_processor(test_block_wake_yield, cpu_id, cpu_num);
 }
 
 #[no_mangle]
 fn main_secondary(cpu_id: usize) {
-    init_secondary_processor(cpu_id);
+    trap_handler::init_secondary_processor();
+    task_management::init_secondary_processor(cpu_id);
 }
 
 static BLOCK_QUEUE: LazyInit<SpinNoIrq<BlockQueue>> = LazyInit::new();
 
 fn test_block_wake_yield() -> i32 {
+
+    // println!("interrupt enable status:");
+    // println!("    sstatus.sie: {}", sstatus::read().sie());
+    // println!("    sie.sext: {}", sie::read().sext());
+    // println!("    sie.ssoft: {}", sie::read().ssoft());
+    // println!("    sie.stimer: {}", sie::read().stimer());
+
+    // unsafe { asm!("ebreak"); }
+
     BLOCK_QUEUE.init_by(SpinNoIrq::new(BlockQueue::new()));
     for i in 0 .. 2 {
         spawn_to_global(|| {
@@ -67,7 +92,7 @@ fn test_block_wake_yield() -> i32 {
             let cpu_id_3 = current_processor_id();
             warn!("after wake, coroutine {task_id} is running on cpu {cpu_id_3}");
 
-            exit_current_async(0);
+            exit_current_async(0).await;
             -1
         });
     }
